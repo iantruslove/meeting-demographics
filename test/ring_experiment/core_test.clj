31a009ea3f364fc9318feda193577241524bc259
Ian Truslove <ian.truslove@gmail.com>
2013-06-04 08:36:31 -0600

    new project

---
+++ test/ring_experiment/core_test.clj
@@ -0,0 +1,7 @@
+(ns ring-experiment.core-test
+  (:require [clojure.test :refer :all]
+            [ring-experiment.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
