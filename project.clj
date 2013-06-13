43f1fd497b358d5dd70fcb860e585da06ec76a4a
Ian Truslove <ian.truslove@gmail.com>
2013-06-12 20:13:42 -0600

    Oops

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  [ring "1.1.8"]
                  ;[ring/ring-json "0.2.0"]
                  [ring-middleware-format "0.3.0"]
-                 [cljsbuild "0.3.2"]]
+                 [cljsbuild "0.3.2"]
                  ;; CLJS:
                  [cljsbuild "0.3.2"]
                  [domina "1.0.2-SNAPSHOT"]]
