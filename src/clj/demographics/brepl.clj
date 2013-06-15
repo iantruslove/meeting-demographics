7eea81da1bfb1127ae6dd97767880d61373ccb4f
Ian Truslove <ian.truslove@gmail.com>
2013-06-15 12:27:49 -0600

    Got the right side of the connection code

--- src/clj/demographics/brepl.clj
+++ src/clj/demographics/brepl.clj
@@ -1,10 +1,7 @@
 (ns demographics.brepl
-  (:require [clojure.browser.repl :as brepl]))
+  (:require [cljs.repl.browser :as brepl]))
 
 (defn connect []
-  (brepl/connect "http://localhost:9000/repl")
-
-  (cemerick.piggieback/cljs-repl :repl-env (doto (cljs.repl.browser/repl-env
-                                                  :port 9000)
-                                             cljs.repl/-setup))
-)
+  (cemerick.piggieback/cljs-repl
+   :repl-env (doto (cljs.repl.browser/repl-env :port 9000)
+               cljs.repl/-setup)))
