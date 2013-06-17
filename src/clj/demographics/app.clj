0da5f5b9282bcebe55574e4adeee9006708e40e8
Ian Truslove <ian.truslove@gmail.com>
2013-06-16 22:52:43 -0600

    Moved form into templates dir, and serving it out with a 6-char sub-path

--- src/clj/demographics/app.clj
+++ src/clj/demographics/app.clj
@@ -12,7 +12,7 @@
 (defn add-participant [attendees gender type]
         (update-in attendees [gender type] inc)) 
 
-(defn register-participant [gender type]
+(defn register-participant! [gender type]
   (swap! meeting-attendees add-participant gender type))
 
 (defroutes api-routes
@@ -21,14 +21,16 @@
         (fn [request]
           (let [gender (keyword (get-in request [:body :data-primary-val]))
                 type (keyword (get-in request [:body :data-secondary-val]))]
-            (resp/response (register-participant gender type))))))
+            (resp/response (register-participant! gender type))))))
 
 (defroutes app-routes
   (context "/api" [] (-> api-routes
                          (wrap-json-response)
                          (wrap-json-body {:keywords? true})))
-  (resources "/")
   (GET "/" [] (resp/resource-response "index.html" {:root "public"}))
+  (GET ["/:id" :id #"[a-zA-Z0-9]{6,6}"] [id]
+       (resp/resource-response "form.html" {:root "templates"}))
+  (resources "/")
   (not-found "<p>404 - page not found</p>\n"))
 
 (def app
